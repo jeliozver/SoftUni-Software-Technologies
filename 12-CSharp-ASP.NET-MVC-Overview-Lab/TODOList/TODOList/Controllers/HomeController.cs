0e0bd9be0cf35ab55c1e7a69d1afa81aa7819835
Jeliozver <jelioradoev@gmail.com>
2017-12-03 10:29:43 +0200

    Added solutions for CSharp lab tasks

---
+++ 12-CSharp-ASP.NET-MVC-Overview-Lab/TODOList/TODOList/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using TODOList.Models;
+
+namespace TODOList.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            using (var db = new TaskDbContext())
+            {
+                var tasks = db.Tasks.ToList();
+
+                return View(tasks);
+            }
+        }
+    }
+}
